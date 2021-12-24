eed77210a8182e166378eb9dce485f20c96db582
Maikaze0315 <51293101+Maikaze0315@users.noreply.github.com>
2021-12-24 18:36:15 +0900

    Update filesaver.py
    フォルダのパスを直接コードに埋め込むのではなく、最初にフォルダを参照できるように
    処理が終わり次第、zipファイルに圧縮するように

--- filesaver.py
+++ filesaver.py
@@ -1,19 +1,33 @@
 import os
 import shutil
+import tkinter
+from tkinter import filedialog
 
-x = int(input())
+print("Progのフォルダがある場所を選択してください")
+dire = "C://download"
+fld = filedialog.askdirectory(initialdir = dire)
 
-os.mkdir("C:/Users/314Cl/Google ドライブ/Prog1/Prog1_" + str(x))
-dir = "C:/Users/314Cl/Google ドライブ/Prog1/Prog1_" + str(x)
-files1 = os.listdir("C:/Users/314Cl/Google ドライブ/Prog1")
+print("第何回か入力してください（数字のみ）")
+n = input()
+
+dir = fld + "_" + n
+
+files1 = os.listdir(fld)
+
+os.mkdir(fld + "/Prog1_" + n)
 
 for file in files1:
-    if "1_" + str(x) in file:
+    if "1_" + n in file:
         if "hw" in file:
-            shutil.copy("C:/Users/314Cl/Google ドライブ/Prog1/" + file + "/Debug/" + file + ".exe",   "C:/Users/314Cl/Google ドライブ/Prog1/Prog1_" + str(x))
-            shutil.copy("C:/Users/314Cl/Google ドライブ/Prog1/" + file + "/" + file + "/" + file + ".c",   "C:/Users/314Cl/Google ドライブ/Prog1/Prog1_" + str(x))
+            shutil.copy("{}/{}/Debug/{}.exe".format(fld, file, file), "{}/prog1_{}".format(fld, n))
+            shutil.copy("{}/{}/{}/{}.c".format(fld, file, file, file), "{}/Prog1_{}".format(fld, n))
 
-    if "1_" + str(x+1) in file:
+    if "1_" + str(int(n) + 1) in file:
         if "pre" in file:
-            shutil.copy("C:/Users/314Cl/Google ドライブ/Prog1/" + file + "/Debug/" + file + ".exe",   "C:/Users/314Cl/Google ドライブ/Prog1/Prog1_" + str(x))
-            shutil.copy("C:/Users/314Cl/Google ドライブ/Prog1/" + file + "/" + file + "/" + file + ".c",   "C:/Users/314Cl/Google ドライブ/Prog1/Prog1_" + str(x))
+            shutil.copy("{}/{}/Debug/{}.exe".format(fld, file, file), "{}/prog1_{}".format(fld, n))
+            shutil.copy("{}/{}/{}/{}.c".format(fld, file, file, file), "{}/Prog1_{}".format(fld, n))
+
+print("只今圧縮中のため、少々お待ちください。")
+shutil.make_archive('1_{}'.format(n), 'zip', root_dir=fld)
+    
+print("完了しました。選択したディレクトリをご確認ください。")
